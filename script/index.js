3bfd64200caed2fdb51ebc57bc50d4b5629cd132
Ingrid <ingrid-almeida-developer@users.noreply.github.com>
2025-07-25 15:31:27 -0300

    Muda o arquivo javascript

---
+++ script/index.js
@@ -0,0 +1,11 @@
+function main() {
+ get_json();
+}
+
+function get_json() {
+ let text_h1 = document.getElementById("value");
+ fetch("https://raw.githubusercontent.com/henriquepb/populacao-brasil-api/main/populacao.json");
+ .then(resposta => resposta.json());
+ .then(dados => { text_h1.innerText = dados.populacao.toLocaleString("pt-BR");});
+ .catch(erro => console.error("Erro em carregar dados", erro);
+}
