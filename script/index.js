1add80425d96d7cf77bf6255fe89c413cbe6a294
Ingrid <ingrid-almeida-developer@users.noreply.github.com>
2025-07-26 13:59:27 -0300

    Mudar js 2

--- script/index.js
+++ script/index.js
@@ -7,5 +7,5 @@ function get_json() {
  fetch("https://raw.githubusercontent.com/henriquepb/populacao-brasil-api/main/populacao.json")
  .then(resposta => resposta.json())
  .then(dados => { text_h1.innerHTML = dados.populacao.valor.toLocaleString("pt-BR");});
- .catch(erro => console.error("Erro em carregar dados", erro);
+ .catch(erro => console.error("Erro em carregar dados", erro));
 }
