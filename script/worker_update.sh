48262d601bc02ffb413cc52713244f2c4b835cae
Ingrid <ingrid-almeida-developer@users.noreply.github.com>
2025-07-26 20:14:58 -0300

    Mudando o script

--- script/worker_update.sh
+++ script/worker_update.sh
@@ -1,10 +1,15 @@
-function main() {
+#!/bin/bash
 
+function main() {
+ changeHtml
 }                                                                                                           function get_json() {                                  local api_json=$(curl -s "https://raw.githubusercontent.com/henriquepb/populacao-brasil-api/main/populacao.json" | jq '.populacao.valor')
- echo $api_json                                       }            
+ echo $api_json   
+}
 
 function changeHtml() {
  readonly local file=~/html/pasta/index.html
  local _value=$(get_json)
- local command_sed=$(sed -i 's/[0-9]\{3,\}/'"$_value"'/g' $file:)
+ local command_sed=$(sed -i 's/[0-9]\{3,\}/'"$_value"'/g' $file )
 }
+
+main
