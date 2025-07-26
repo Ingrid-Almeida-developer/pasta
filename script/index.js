1dc3405416462a6c0e423de9ef767a494faad529
Ingrid <ingrid-almeida-developer@users.noreply.github.com>
2025-07-26 13:57:33 -0300

    Mudar js

--- script/index.js
+++ script/index.js
@@ -4,8 +4,8 @@ function main() {
 
 function get_json() {
  let text_h1 = document.getElementById("value");
- fetch("https://raw.githubusercontent.com/henriquepb/populacao-brasil-api/main/populacao.json");
- .then(resposta => resposta.json());
- .then(dados => { text_h1.innerHTML = dados.populacao.toLocaleString("pt-BR");});
+ fetch("https://raw.githubusercontent.com/henriquepb/populacao-brasil-api/main/populacao.json")
+ .then(resposta => resposta.json())
+ .then(dados => { text_h1.innerHTML = dados.populacao.valor.toLocaleString("pt-BR");});
  .catch(erro => console.error("Erro em carregar dados", erro);
 }
