2157ca90aa56036f0b63e5221d60494076f18128
Ingrid <ingrid-almeida-developer@users.noreply.github.com>
2025-07-27 17:22:17 -0300

    chamando a função corretamente

--- script/index.js
+++ script/index.js
@@ -2,6 +2,8 @@ function get_json() {
  let text_h1 = document.getElementById("value");
  fetch("https://raw.githubusercontent.com/henriquepb/populacao-brasil-api/main/populacao.json")
  .then(resposta => resposta.json())
- .then(dados => { text_h1.innerHTML = dados.populacao.valor.toLocaleString("pt-BR");});
+ .then(dados => { document.getElementById("value").innerHTML = dados.populacao.valor.toLocaleString("pt-BR");});
  .catch(erro => console.error("Erro em carregar dados", erro));
 }
+
+get_json();
