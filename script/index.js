f71bbc3a33502915de92d176bcd86812453f4f6c
Ingrid <ingrid-almeida-developer@users.noreply.github.com>
2025-08-03 14:40:02 -0300

    Mudando encadeamento do catch

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 function get_json() {
  fetch("https://api.open-meteo.com/v1/forecast?latitude=-23.55&longitude=-46.63&current_weather=true")
  .then(resposta => resposta.json())
- .then(dados => { document.getElementById("value").innerHTML = dados.current_weather.temperature.toLocaleString("pt-BR");});
+ .then(dados => { document.getElementById("value").innerHTML = dados.current_weather.temperature.toLocaleString("pt-BR");})
  .catch(erro => console.error("Erro em carregar dados", erro));
 }
 
